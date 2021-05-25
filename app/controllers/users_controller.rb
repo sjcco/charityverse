97986e62f99f95396b583238cf93045015508d10
CarlosC <sjcco@gmail.com>
2021-05-25 14:53:24 -0500

    Add layout and styling for login and sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
   end
 
   def create
+    @user = User.new(user_params)
+    @user.save
+    session[:user_id] = @user.id
+    flash.notice = "User #{@user.name} has succesfully signed in"
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root 'sessions#index'
+  get 'login' => 'sessions#new', as: 'login'  
+  get 'logout' => 'sessions#destroy', as: 'logout'
+  get 'signup' => 'users#new', as: 'signup'
+
+  resources :sessions, only: [:create]
+  resources :users, only: [:create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
