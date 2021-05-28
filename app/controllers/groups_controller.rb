eb9864f545daa8f01c95b4fa59c9b41b108a5a80
CarlosC <sjcco@gmail.com>
2021-05-28 02:24:46 -0500

    Created form to add new group and display individual groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,40 @@
 class GroupsController < ApplicationController
-  def new; end
+  def new
+    @title = 'New Group'
+    @group = Group.new
+  end
 
-  def show; end
+  def create
+    @group = Group.new(group_params)
+
+    if @group.save
+      flash[:notice] = "Group #{@group.name} was succesfully created!"
+      redirect_to @group
+    else
+      flash[:alert] = if @group.errors.nil?
+                        'Something went wrong, please try again'
+                      else
+                        @group.errors.messages
+                      end
+      render 'new'
+    end
+  end
+
+  def show
+    @group = Group.find(params[:id])
+    @title = "#{@group.name} transactions"
+    @charities = Charity.where(group_id: @group.id).all
+  end
 
   def index; end
 
   def edit; end
 
   def destroy; end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon, :user_id)
+  end
 end
